3430f1be40f9f6a587ac1e087b2a09c03d7e0657
Modzeleczek <lukasz.modzelewski1@gmail.com>
2023-04-15 03:07:51 +0200

    feat: Add grade email sender's static localization

--- Gradebook/Utils/PeriodicGradeSheet/GradeEmailJob.cs
+++ Gradebook/Utils/PeriodicGradeSheet/GradeEmailJob.cs
@@ -1,7 +1,11 @@
-﻿using Gradebook.Models;
+﻿#define ENGLISH
+using Gradebook.Models;
 using Gradebook.Models.ViewModels;
 using Quartz;
 using System;
+#if !ENGLISH
+using System.Collections.Generic;
+#endif
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,17 +19,17 @@ namespace Gradebook.Utils.PeriodicGradeSheet
         {
             var studentSearch = Db.Student.Where(e => e.ParentId == parent.Id);
             if (studentSearch.Count() == 0)
-                return $"Rodzic {parent.ApplicationUser.Name} {parent.ApplicationUser.Surname} nie posiada dzieci.";
+                return $"{D("Parent")} {parent.ApplicationUser.Name} {parent.ApplicationUser.Surname} {D("does not have children.")}";
             var students = studentSearch.ToArray();
             var sb = new System.Text.StringBuilder();
-            sb.Append($"Zestawienie ocen dzieci rodzica {parent.ApplicationUser.Name} {parent.ApplicationUser.Surname} w okresie do {date}.<br/><br/>");
+            sb.Append($"{D("Grade report of the children of parent")} {parent.ApplicationUser.Name} {parent.ApplicationUser.Surname} {D("to")} {date}.<br/><br/>");
             foreach (var s in students)
             {
                 var grades = Db.Grade.Where(e => e.StudentId == s.Id).ToArray();
                 sb.Append($"<h2>{s.ApplicationUser.Name} {s.ApplicationUser.Surname}</h2>");
                 if (s.ClassId == null)
                 {
-                    sb.Append($"<h4>Brak klasy</h4>");
+                    sb.Append($"<h4>{D("No class")}</h4>");
                     continue;
                 }
                 var subjects = s.Class.TeacherClassSubjects.Select(e => e.Subject).ToArray();
@@ -35,17 +39,17 @@ namespace Gradebook.Utils.PeriodicGradeSheet
                     sb.Append($"<h4>{sg.SubjectName}</h4>");
                     if (sg.Grades.Count == 0)
                     {
-                        sb.Append("Brak ocen");
+                        sb.Append(D("No grades"));
                         continue;
                     }
                     sb.Append("<table>");
                     sb.Append("<tr>");
-                    sb.Append("<th>Wartość</th>");
-                    sb.Append("<th>Waga</th>");
-                    sb.Append("<th>Komentarz</th>");
-                    sb.Append("<th>Czas modyfikacji</th>");
-                    sb.Append("<th>Imię nauczyciela</th>");
-                    sb.Append("<th>Nazwisko nauczyciela</th>");
+                    sb.Append($"<th>{D("Value")}</th>");
+                    sb.Append($"<th>{D("Weight")}</th>");
+                    sb.Append($"<th>{D("Comment")}</th>");
+                    sb.Append($"<th>{D("Modification time")}</th>");
+                    sb.Append($"<th>{D("Teacher name")}</th>");
+                    sb.Append($"<th>{D("Teacher surname")}</th>");
                     sb.Append("</tr>");
                     foreach (var grade in sg.Grades)
                     {
@@ -69,13 +73,36 @@ namespace Gradebook.Utils.PeriodicGradeSheet
             var parents = Db.Parent.ToArray();
             var today = DateTime.Now;
             var date = $"{today.Day}.{today.Month}.{today.Year}";
-            var subject = "Zestawienie ocen - " + date;
+            var subject = D("Grade report - ") + date;
             foreach (var parent in parents)
             {
                 var html = GenerateGradeSheet(parent, date);
                 EmailSender.Send("noreply@gradebook.com", parent.ApplicationUser, subject, html, null, true);
             }
             return null;
+        }
+
+        private string D(string key)
+        {
+#if ENGLISH
+            return key;
+#else
+            var d = new Dictionary<string, string>();
+            d["Parent"] = "Rodzic";
+            d["does not have children."] = "nie posiada dzieci.";
+            d["Grade report of the children of parent"] = "Zestawienie ocen dzieci rodzica";
+            d["to"] = "w okresie do";
+            d["No class"] = "Brak klasy";
+            d["No grades"] = "Brak ocen";
+            d["Value"] = "Wartość";
+            d["Weight"] = "Waga";
+            d["Comment"] = "Komentarz";
+            d["Modification time"] = "Czas modyfikacji";
+            d["Teacher name"] = "Imię nauczyciela";
+            d["Teacher surname"] = "Nazwisko nauczyciela";
+            d["Grade report - "] = "Zestawienie ocen - ";
+            return d[key];
+#endif
         }
     }
 }
